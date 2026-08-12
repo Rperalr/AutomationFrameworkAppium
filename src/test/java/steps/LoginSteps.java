ce713567377a02a512d6149f0a77e6b908432d71
Rperalt <rperalt@emeal.nttdata.com>
2026-08-11 18:19:29 -0600

    Se agregan cambios

--- src/test/java/steps/LoginSteps.java
+++ src/test/java/steps/LoginSteps.java
@@ -53,10 +53,4 @@ public class LoginSteps extends BaseTest {
         System.out.println("Validando lbl cuenta ✅");
     }
 
-    @Then("login Succes usuario {string} y password {string}")
-    public void login_succees_usuario_string_y_password_string(String username, String password) {
-        loginFlow().login(username, password);
-        System.out.println("Validando lbl cuenta ✅");
-    }
-
 }
